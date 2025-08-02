0fd7666cf84629338399f4f57a135eac76c98ce0
bundlebooth <hello@bundlebooth.ca>
2025-08-01 21:23:56 -0400

    Update 18_ Audit Triggers.sql

--- backend/dbscripts/18_ Audit Triggers.sql
+++ backend/dbscripts/18_ Audit Triggers.sql
@@ -7,23 +7,20 @@ AFTER INSERT, UPDATE, DELETE
 AS
 BEGIN
     SET NOCOUNT ON;
-GO
+    
     DECLARE @ActionType NVARCHAR(50);
-GO
+    
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
         SET @ActionType = 'UPDATE';
-GO
     ELSE IF EXISTS (SELECT * FROM inserted)
         SET @ActionType = 'INSERT';
-GO
     ELSE
         SET @ActionType = 'DELETE';
-GO
+    
     -- Get current user from application context or system user
     DECLARE @CurrentUser NVARCHAR(128);
-GO
     SET @CurrentUser = ISNULL(CAST(CONTEXT_INFO() AS NVARCHAR(128)), SYSTEM_USER);
-GO
+    
     -- Log changes
     INSERT INTO AuditLogs (UserID, ActionType, TableName, RecordID, OldValues, NewValues, IPAddress)
     SELECT 
@@ -39,7 +36,6 @@ GO
         FULL OUTER JOIN deleted d ON i.UserID = d.UserID
     WHERE 
         (i.UserID IS NOT NULL OR d.UserID IS NOT NULL);
-GO
 END;
 GO
 
@@ -50,23 +46,20 @@ AFTER INSERT, UPDATE, DELETE
 AS
 BEGIN
     SET NOCOUNT ON;
-GO
+    
     DECLARE @ActionType NVARCHAR(50);
-GO
+    
     IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
         SET @ActionType = 'UPDATE';
-GO
     ELSE IF EXISTS (SELECT * FROM inserted)
         SET @ActionType = 'INSERT';
-GO
     ELSE
         SET @ActionType = 'DELETE';
-GO
+    
     -- Get current user from application context or system user
     DECLARE @CurrentUser NVARCHAR(128);
-GO
     SET @CurrentUser = ISNULL(CAST(CONTEXT_INFO() AS NVARCHAR(128)), SYSTEM_USER);
-GO
+    
     -- Log changes
     INSERT INTO AuditLogs (UserID, ActionType, TableName, RecordID, OldValues, NewValues, IPAddress)
     SELECT 
@@ -82,7 +75,5 @@ GO
         FULL OUTER JOIN deleted d ON i.BookingID = d.BookingID
     WHERE 
         (i.BookingID IS NOT NULL OR d.BookingID IS NOT NULL);
-GO
 END;
 GO
-
