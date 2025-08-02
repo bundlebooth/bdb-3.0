6cf1e80753834efe514fb742608cbb13c2d05175
bundlebooth <hello@bundlebooth.ca>
2025-08-02 01:34:25 -0400

    Update bookings.js

--- venuevue-api/src/routes/bookings.js
+++ venuevue-api/src/routes/bookings.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { poolPromise, sql } = require('../config/db');
 const { authenticate } = require('../middlewares/auth');
+const sql = require('mssql');  
 
 // Create a booking
 router.post('/', authenticate, async (req, res) => {
