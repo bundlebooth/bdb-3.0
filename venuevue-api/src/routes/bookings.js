97c610e243205fd66d5f28d66e13157ba95d2d98
bundlebooth <hello@bundlebooth.ca>
2025-10-04 15:36:47 -0400

    Update bookings.js

--- venuevue-api/src/routes/bookings.js
+++ venuevue-api/src/routes/bookings.js
@@ -392,6 +392,8 @@ router.get('/services/:categoryId', async (req, res) => {
       INNER JOIN VendorProfiles vp ON vc.VendorProfileID = vp.VendorProfileID
       WHERE vc.Category LIKE '%' + @Category + '%'
         AND vp.IsCompleted = 1
+        AND vp.AcceptingBookings = 1
+        AND vp.StripeAccountID IS NOT NULL
       ORDER BY vp.BusinessName
     `);
 
