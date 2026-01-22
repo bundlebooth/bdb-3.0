42603b27676c0e116af95e940a90ad1ba8d7ce7a
bundlebooth <hello@bundlebooth.ca>
2026-01-22 11:19:03 -0500

    Update cu_600_037_sp_GetEmailTemplate.sql

--- backend/Migration/07_StoredProcedures/cu_600_037_sp_GetEmailTemplate.sql
+++ backend/Migration/07_StoredProcedures/cu_600_037_sp_GetEmailTemplate.sql
@@ -30,10 +30,10 @@ BEGIN
            h.HtmlContent AS HeaderHtml, h.TextContent AS HeaderText,
            b.HtmlContent AS BodyHtml, b.TextContent AS BodyText,
            f.HtmlContent AS FooterHtml, f.TextContent AS FooterText
-    FROM EmailTemplates t
-    LEFT JOIN EmailTemplateComponents h ON t.HeaderComponentID = h.ComponentID
-    INNER JOIN EmailTemplateComponents b ON t.BodyComponentID = b.ComponentID
-    LEFT JOIN EmailTemplateComponents f ON t.FooterComponentID = f.ComponentID
+    FROM admin.EmailTemplates t
+    LEFT JOIN admin.EmailTemplateComponents h ON t.HeaderComponentID = h.ComponentID
+    INNER JOIN admin.EmailTemplateComponents b ON t.BodyComponentID = b.ComponentID
+    LEFT JOIN admin.EmailTemplateComponents f ON t.FooterComponentID = f.ComponentID
     WHERE t.TemplateKey = @TemplateKey AND t.IsActive = 1 AND b.IsActive = 1
       AND (h.IsActive = 1 OR h.ComponentID IS NULL) AND (f.IsActive = 1 OR f.ComponentID IS NULL);
 END
